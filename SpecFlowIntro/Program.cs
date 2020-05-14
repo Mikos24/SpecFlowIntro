d5657e1bb3e2445928960de7c8e598f66b12737e
Mikos_24 <michal.swierkosz@googlemail.com>
2020-05-14 23:08:32 +0100

    Anna's Specflow project First few configuration files

---
+++ SpecFlowIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SpecFlowIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Your name is " + name);
+            Console.ReadLine();
+        }
+    }
+}
